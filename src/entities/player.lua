154caf4d78c48407e983e0fa0b5a6c108cf6519b
Orelavant <jacobkvalero@gmail.com>
2024-12-27 22:30:11 -0500

    tail moves back half of body, circles sort of don't bump against each other

--- src/entities/player.lua
+++ src/entities/player.lua
@@ -8,26 +8,32 @@ local Player = Object:extend()
 
 -- Config
 Player.radius = 25
-Player.speed = 200
+Player.speed = 250
 Player.color = Orange
 Player.bodyAccelDiv = 100
-Player.tailAccel = Player.radius / Player.bodyAccelDiv
+Player.tailAccel = Player.bodyAccelDiv / (Player.radius * 16)
 Player.tailRangeDiv = 5
-Player.chainCount = 10
+Player.hooverRadius = 25
+Player.chainCount = 2
+Player.headFollowerCount = math.ceil(Player.chainCount * 0.4)
 Player.startingChainSpeed = 1500
 Player.chainSpeedReduction = (Player.startingChainSpeed / Player.chainCount)
-Player.chainSpeedReductionOffset = 15
+Player.chainSpeedReductionOffset = (150 / Player.chainCount) + (Player.radius / 5)
 Player.chainColorAddition = 0.05
 
 ---Constructor
 function Player:new(x, y)
+    self.chainCount = Player.chainCount
+
 	-- Head values
-	self.headX = x
-	self.headY = y
-	self.headDx = 0
-	self.headDy = 0
-	self.nonZeroDx = 0
-	self.nonZeroDy = -1
+	self.hX = x
+	self.hY = y
+	self.hNonZeroDx = 0
+	self.hNonZeroDy = -1
+
+    -- Tail values
+	self.tNonZeroDx = 0
+	self.tNonZeroDy = -1
 
 	-- Create chain
 	self.chain = {}
@@ -35,14 +41,20 @@ function Player:new(x, y)
 end
 
 function Player:update(dt)
+    -- Update front half to follow head
 	for i=1,#self.chain do
         if i == 1 then
             -- Get inputs and update head and tail accordingly
             self:updateHead(self.chain[i], dt)
             self:updateTail(self.chain[#self.chain], dt)
         else
-			-- Update to follow head
-			self:constrainCircleToRadius(self.chain[i - 1], self.chain[i], dt)
+            -- Update to follow head
+            self:constrainCircleToRadius(self.chain[i - 1], self.chain[i], self.hNonZeroDx, self.hNonZeroDy, dt)
+
+            -- Update back half to follow tail
+            if i > Player.headFollowerCount then
+                self:constrainCircleToRadius(self.chain[i - 1], self.chain[i], -self.tNonZeroDx, -self.tNonZeroDy, dt)
+            end
 		end
 	end
 end
@@ -51,13 +63,17 @@ function Player:draw()
 	-- Draw circles
 	for i, circle in ipairs(self.chain) do
 		circle:draw()
+
+        if i == #self.chain then
+            love.graphics.circle("line", circle.x, circle.y, Player.hooverRadius)
+        end
 	end
 end
 
-function Player:constrainCircleToRadius(circle1, circle2, dt)
+function Player:constrainCircleToRadius(circle1, circle2, nonZeroDx, nonZeroDy, dt)
 	-- Get target
-	local behindX = circle1.x + circle1.radius * -self.nonZeroDx
-	local behindY = circle1.y + circle1.radius * -self.nonZeroDy
+	local behindX = circle1.x + circle1.radius * -nonZeroDx
+	local behindY = circle1.y + circle1.radius * -nonZeroDy
 
 	-- Get angle and angle components to target
 	local angle = utils.getSourceTargetAngle(circle2.x, circle2.y, behindX, behindY)
@@ -70,6 +86,19 @@ function Player:constrainCircleToRadius(circle1, circle2, dt)
 	circle2.x = circle2.x + circle2.speed * cos * accel * dt
 	circle2.y = circle2.y + circle2.speed * sin * accel * dt
 
+    -- TODO
+    -- Clamp distance between circles
+    local dist = utils.getDistance(circle1.x, circle1.y, circle2.x, circle2.y)
+    if dist + 1 < circle1.radius + 0 then
+        local dx, dy = circle2.x - circle1.x, circle2.y - circle1.y
+        local clampX, clampY = dx / dist, dy / dist
+		circle2.x = circle1.x + circle1.radius * clampX
+		circle2.y = circle1.y + circle1.radius * clampY
+    end
+
+    -- Handle screen collision
+    circle2:handleScreenCollision()
+
 	return circle2
 end
 
@@ -81,7 +110,7 @@ function Player:initChain()
 	local chainCount = Player.chainCount - 2
 
     -- Add head
-	self.head = CircleInit(self.headX, self.headY, self.headDx, self.headDy, Player.radius, Player.speed, Player.color)
+	self.head = CircleInit(self.hX, self.hY, 0, 0, Player.radius, Player.speed, Player.color)
     table.insert(self.chain, self.head)
 
 	-- Populate chain
@@ -128,7 +157,7 @@ function Player:updateHead(circle, dt)
 
 	-- Update last nonzero dx dy
 	if dx ~= 0 or dy ~= 0 then
-		self.nonZeroDx, self.nonZeroDy = dx, dy
+		self.hNonZeroDx, self.hNonZeroDy = dx, dy
 	end
 
 	-- Update dx, dy
@@ -146,12 +175,23 @@ function Player:updateTail(circle, dt)
 	local angle = utils.getSourceTargetAngle(circle.x, circle.y, mouseX, mouseY)
 	local cos, sin = math.cos(angle), math.sin(angle)
 
+	-- Update last nonzero dx dy
+	if cos ~= 0 or sin ~= 0 then
+		self.tNonZeroDx, self.tNonZeroDy = cos, sin
+	end
+
 	-- Acceleration based off distance to target
-	-- local accel = (utils.getDistance(circle.x, circle.y, mouseX, mouseY) / Player.tailRangeDiv) / Player.tailAccelDiv
+	local accel = math.min(utils.getDistance(circle.x, circle.y, mouseX, mouseY) / Player.bodyAccelDiv, Player.tailAccel)
+    if accel < 0.2 then
+        accel = 0
+    end
 
 	-- Update circle2 position
-	circle.x = circle.x + circle.speed * cos * Player.tailAccel * dt
-	circle.y = circle.y + circle.speed * sin * Player.tailAccel * dt
+	circle.x = circle.x + circle.speed * cos * accel * dt
+	circle.y = circle.y + circle.speed * sin * accel * dt
+
+    -- Handle screen collision
+    circle:handleScreenCollision()
 end
 
 return Player
